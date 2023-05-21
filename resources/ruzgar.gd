8ce3c4ccf3de40eceebe5f05e5e9d32e004fb813
Peter DeVita <mewtwo2643@yahoo.com>
2023-05-21 18:26:48 -0400

    Ruzgar schmooves on down main street

--- resources/ruzgar.gd
+++ resources/ruzgar.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 var isRunning = true
 @export var base_speed = 0.25
+@export var jump_force = 9
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -13,7 +15,14 @@ func _process(delta):
 	pass
 	
 func _physics_process(delta):
-	global_position.x += base_speed
+	var newVel = Vector3(base_speed, -9, 0)
+	if Input.is_action_pressed("game_jump") and is_on_floor():
+		velocity.y = jump_force
+		newVel.y += jump_force
+		
+	velocity = velocity.lerp(newVel, 0.1)
+		
+	print(global_position)
 	if isRunning:
 		move_and_slide()
-	
+	global_position.y = max(global_position.y, -2)
