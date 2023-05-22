4ee1ba57c44ae946318db8f8975f2d171668ed0d
Peter DeVita <mewtwo2643@yahoo.com>
2023-05-21 21:13:41 -0400

    Improve camera and player movement and controls

--- resources/ruzgar.gd
+++ resources/ruzgar.gd
@@ -2,27 +2,38 @@ extends CharacterBody3D
 
 var isRunning = true
 @export var base_speed = 0.25
-@export var jump_force = 9
+@export var jump_force = 9.0
+@export var gravity = 9.0
+@export var left_lane = -9.0
+@export var lane_size = 10.0
+@export var lane = 0
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_lane_position()
 
+func set_lane_position():
+	global_position.z = lane * lane_size + left_lane
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("game_left"):
+		lane -= 1
+		set_lane_position()
+	if Input.is_action_just_pressed("game_right"):
+		lane += 1
+		set_lane_position()
 	
 func _physics_process(delta):
-	var newVel = Vector3(base_speed, -9, 0)
+	var newVel = Vector3(base_speed, gravity, 0)
 	if Input.is_action_pressed("game_jump") and is_on_floor():
 		velocity.y = jump_force
 		newVel.y += jump_force
 		
 	velocity = velocity.lerp(newVel, 0.1)
 		
-	print(global_position)
+	print(velocity)
 	if isRunning:
 		move_and_slide()
 	global_position.y = max(global_position.y, -2)
