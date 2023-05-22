f193e939c71ac17a838f8967e5238b1847d7d714
Gonderage <78834252+Gonderage@users.noreply.github.com>
2023-05-21 21:10:43 -0400

    Add 3D model

--- resources/ruzgar.gd
+++ resources/ruzgar.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 var isRunning = true
-@export var base_speed = 0.25
+@export var base_speed = .25
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
